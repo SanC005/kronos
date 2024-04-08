14745c364e55abc86d8d14f0610d34083dabcb5e
SanC005 <sandeepcsrao@gmail.com>
2024-04-09 02:25:33 +0530

    Add Events Handler
    Handle message and connection update events.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import makeWASocket, { DisconnectReason } from "@whiskeysockets/baileys";
+import { Boom } from "@hapi/boom";
 const makeWAsocket = require("@adiwajshing/baileys").default;
 const {
   DisconnectReason,
@@ -15,10 +17,35 @@ const getMessage = (key) => {
 async function chronosBot() {
   const { state, saveCreds } = await useMultiFileAuthState("auth");
   const socket = makeWAsocket({
-    version: [2,2323,4],
+    version: [2, 2323, 4],
     printQRInTerminal: true,
     auth: state,
     getMessage,
+  });
+
+  socket.ev.process(async (events) => {
+    if (events["connection.update"]) {
+      const { connection, lastDisconnect } = events["connection.update"];
+      if (connection === "close") {
+        if (
+          lastDisconnect?.error?.output?.StatusCode !=
+          DisconnectReason.loggedOut
+        ) {
+          chronosBot();
+        } else {
+          console.log("Disconnected becuase user has logged out");
+        }
+      }
+    }
+    if (events["creds.update"]) {
+      await saveCreds();
+    }
+    if (events["messages.upsert"]) {
+      const { messages } = events["messages.upsert"];
+      messages.forEach((message) => {
+        console.log(message);
+      });
+    }
   });
 }
 
